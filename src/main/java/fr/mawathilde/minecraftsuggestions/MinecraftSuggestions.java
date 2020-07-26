fd6eff3f7ae3dfad2f13bce9c9eafbf7e227e204
Mawathilde <mawathilde@gmail.com>
2020-07-26 13:03:17 +0200

    Initial commit

---
+++ src/main/java/fr/mawathilde/minecraftsuggestions/MinecraftSuggestions.java
@@ -0,0 +1,18 @@
+package fr.mawathilde.minecraftsuggestions;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(MinecraftSuggestions.MOD_ID)
+public class MinecraftSuggestions {
+
+    public static final String MOD_ID = "minecraftsuggestions";
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public MinecraftSuggestions() {
+
+    }
+
+}
